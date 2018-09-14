5aa4c67a928db0da956a338b1ac829c66058f917
BaSiczek <barbara.siczek@tooploox.com>
2018-09-14 20:21:43 +0200

    Repare heroReport

--- heroReport.js
+++ heroReport.js
@@ -8,8 +8,7 @@ function heroReportResponce(userId, callback){
   database.getUser(UserId, (user) => {
       var responceMessage = `:space_invader: Hero: ${user.fullName} \
       (${user.roleID})\n:muscle: Level: ${user.level}\n:coin: Exp.: ${user.xp}`
-    }
-      callback (string)
+      callback (responceMessage)
   })
 
 }
