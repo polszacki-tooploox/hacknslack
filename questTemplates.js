6cca45adb906c332a0c386c473e1809bdb8e8dba
BaSiczek <barbara.siczek@tooploox.com>
2018-09-15 12:38:16 +0200

    Add emoticons to templates

--- questTemplates.js
+++ questTemplates.js
@@ -3,12 +3,12 @@ module.exports = {
 };
 
 var templates = {
-    "Alarm" : "All doors locked. The spaceship is safe!",
-    "Documents" : "Message in the bottle read and signed, user documentation completed. Ready to dock.",
-    "Improvements" : "Your Captain appreciates what you’re doing! Join our special forces and help us achieve more. The spaceship accelerates with the speed of sound!",
-    "Delivery" : "Guess what! Special delivery from own friendly space station Tesco has just arrived, time for unloading! Thanks to this, we may survive another week in space.\n\nConsequences of failed mission: starvation",
-    "Event" : "Hands needed, we cannot win the battle solo.",
-    "Interview" : "A castaway from another planet is planning to join our ranks and strengthen the crew. We need a help in verifying that he or she have the necessary powers and skills. It is also obligatory to X-ray his/her personality to check if he/she is not a spy.",
-    "CleaningBig" : "Time to clean the main cabin, we are running out of the oxygen and space to sit.\n\nConsequences of failed mission: bacteria and waste invasion",
-    "CleaningSmall" : "Warning! Achtung! Attenzione! Pozor!\nAlien forms of life will find a wheel: contamination and evacuation danger!"
+    "Alarm" : "All doors locked.:closed_lock_with_key: The spaceship is safe!",
+    "Documents" : "Message in the bottle read and signed, user documentation completed.:page_with_curl: Ready to dock.",
+    "Improvements" : "Your Captain appreciates what you’re doing!:tada: Join our special forces and help us achieve more. The spaceship accelerates with the speed of sound!:rocket:",
+    "Delivery" : "Guess what! Special delivery from own friendly space station Tesco has just arrived, time for unloading! :tesco: Thanks to this, we may survive another week in space.\n\nConsequences of failed mission: starvation",
+    "Event" : "Hands needed, we cannot win the battle solo.:pray:",
+    "Interview" : "A castaway from another planet is planning to join our ranks and strengthen the crew. We need a help in verifying that he or she have the necessary powers and skills. It is also obligatory to X-ray his/her personality to check if he/she is not a spy.:male-detective:",
+    "CleaningBig" : "Time to clean the main cabin, we are running out of the oxygen and space to sit.\n\nConsequences of failed mission: bacteria and waste invasion :hankey:",
+    "CleaningSmall" : "Warning! Achtung! Attenzione! Pozor! :sos:\nAlien forms of life will find a wheel: contamination and evacuation danger!"
 }
