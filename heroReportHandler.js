62d84577f814c0f76c145644dd1729f86cca8c9d
BaSiczek <barbara.siczek@tooploox.com>
2018-09-15 13:34:40 +0200

    Adding templates to quests

--- heroReportHandler.js
+++ heroReportHandler.js
@@ -5,15 +5,9 @@ module.exports = {
 var database = require("./database")
 var questParticipation = require("./questParticipation")
 
-function levelThroshold(lv) {
-    return questPrticipation.xpLevels((element) => {
-        return xp < element
-    }) +1
-}
-
 function heroReportAnswer(req, res, callback){
     let userId = req.body.user_id
-    var max = levelThroshold(user.level)
+    var max = questParticipation.xpLevels(user.level-1)
     database.getUser(userId, (user) => {
         var responceMessage = `:alien: ${user.roleId}: ${user.name} \
         \n:trophy: Level: ${user.level}\n:star2: Exp.: ${user.xp} / ${max}`  
